9e5aea85dc46998ad59f093698e437584fb5fabf
Aidos <aidos_ka@yahoo.com>
2020-08-16 09:58:46 -0500

    add index.js

--- src/components/MovieSearch/index.js
+++ src/components/MovieSearch/index.js
@@ -7,8 +7,10 @@ import api from "api";
 export const MovieSearch = () => {
   const [movies, setMovies] = useState([]);
   const searchHandler = async (event) => {
-    const { results } = await api.index(event.target.elements[0].value);
-    setMovies(results);
+    if (event.target.elements[0].value) {
+      const { results } = await api.index(event.target.elements[0].value);
+      setMovies(results);
+    }
   };
 
   return (
@@ -28,14 +30,20 @@ export const MovieSearch = () => {
         </div>
       </section>
       <footer className="footer">
-  <div className="content has-text-centered">
-    <p>
-      <strong>Application</strong> by <a href="https://jgthms.com">John Doe</a>. This statement meant to be very official and Corporate Looking Ltd.
-      <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>. The website content
-      is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
-    </p>
-  </div>
-</footer>
+        <div className="content has-text-centered">
+          <p>
+            <strong>Application</strong> by{" "}
+            <a href="https://jgthms.com">John Doe</a>. This statement meant to
+            be very official and Corporate Looking Ltd.
+            <a href="http://opensource.org/licenses/mit-license.php"> MIT</a>.
+            The website content is licensed{" "}
+            <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
+              CC BY NC SA 4.0
+            </a>
+            .
+          </p>
+        </div>
+      </footer>
     </body>
   );
 };
